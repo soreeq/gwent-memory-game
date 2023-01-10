c3a2a3dedf1b3c41b18f1dc02fc3e73b2ac8ca25
soreeq <soreeq@gmail.com>
2023-01-10 02:03:33 +0100

    improved functionality

--- memory.js
+++ memory.js
@@ -1,37 +1,20 @@
 var cards = ["ciri.png", "geralt.png", "jaskier.png","jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png" ];
 //alert (card[4]);
 
+cards.sort(function() {
+    return Math.random() - 0.5;
+  });
+
 console.log(cards);
 
-var c0 = document.getElementById('c0');
-var c1 = document.getElementById('c1');
-var c2 = document.getElementById('c2');
-var c3 = document.getElementById('c3');
-
-var c4 = document.getElementById('c4');
-var c5 = document.getElementById('c5');
-var c6 = document.getElementById('c6');
-var c7 = document.getElementById('c7');
-
-var c8 = document.getElementById('c8');
-var c9 = document.getElementById('c9');
-var c10 = document.getElementById('c10');
-var c11 = document.getElementById('c11');
-
-c0.addEventListener("click", function(){    revealCard(0); });
-c1.addEventListener("click", function(){    revealCard(1); });
-c2.addEventListener("click", function(){    revealCard(2); });
-c3.addEventListener("click", function(){    revealCard(3); });
-
-c4.addEventListener("click", function(){    revealCard(4); });
-c5.addEventListener("click", function(){    revealCard(5); });
-c6.addEventListener("click", function(){    revealCard(6); });
-c7.addEventListener("click", function(){    revealCard(7); });
-
-c8.addEventListener("click", function(){    revealCard(8); });
-c9.addEventListener("click", function(){    revealCard(9); });
-c10.addEventListener("click", function(){    revealCard(10); });
-c11.addEventListener("click", function(){    revealCard(11); });
+for (var i = 0; i < 12; i++) {
+    (function(i) {
+      var element = document.getElementById('c' + i);
+      element.addEventListener("click", function() {
+        revealCard(i);
+      });
+    })(i);
+  }
 
 var oneVisible = false;
 var turnCounter = 0;
@@ -83,7 +66,12 @@ function hide2Cards(nr1, nr2){
     pairsLeft--;
 
     if(pairsLeft == 0 ){
-        $('.board').html('<h1> Win! <br> Done in ' + turnCounter + ' turns </h1>');
+        $('.board').html('<br><h1> Win! <br> Done in ' + turnCounter + ' turns </h1>');
+        var resetButton = document.createElement('button');
+        resetButton.innerHTML = 'Reset game';
+        resetButton.addEventListener('click', resetGame);
+        resetButton.addClass('reset-button');
+        document.body.appendChild(resetButton);
     }
 }
 
@@ -99,3 +87,38 @@ function restore2Cards(nr1, nr2){
 
         lock = false;
 }
+
+function resetGame() {
+    // ustaw początkowe wartości zmiennych
+    oneVisible = false;
+    turnCounter = 0;
+    lock = false;
+    pairsLeft = 6;
+    $('.score').html('Turn counter: ' + turnCounter);
+  
+    // przetasuj karty
+    cards.sort(function() {
+      return Math.random() - 0.5;
+    });
+
+    for (var i = 0; i < 12; i++) {
+        (function(i) {
+          var element = document.getElementById('c' + i);
+          element.addEventListener("click", function() {
+            revealCard(i);
+          });
+        })(i);
+      }
+  
+    // ustaw zdjęcia kart na początkowe
+    for (var i = 0; i < 12; i++) {
+      $('#c' + i).css('background-image', 'url(img/karta.png)');
+      $('#c' + i).addClass('card');
+      $('#c' + i).removeClass('cardA');
+      $('#c' + i).css('opacity', '1');
+    }
+  
+    // ustaw tekst na początkowy
+    $('.board').html('');
+  }
+  
